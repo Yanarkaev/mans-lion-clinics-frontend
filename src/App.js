52ad050e461db43d817587269036ccb6e813a903
Yanarkaev <hasssanbek@gmail.com>
2022-11-16 17:15:37 +0300

    Сделал анимацию шапки и недосверстал aboutPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import  './App.scss'
 import Header from './components/Header/Header';
+import AboutPage from './pages/About/AboutPage';
+import Preloader from './components/Preloader/Preloader';
 
 function App() {
   return <div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,42 @@
-import React from "react";
-import { Button, Container, Nav, Navbar, } from "react-bootstrap";
-import styles from "./header.module.scss"
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import styles from "./header.module.scss";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [scrollPos, setScrollPos] = useState("");
+
+  document.addEventListener("scroll", () => {
+    setScrollPos(window.scrollY);
+  });
+
   return (
-    <Navbar className={styles.header}>
+    <Navbar
+      className={`${styles.header} ${scrollPos > 0 ? styles.headerFixed : ""}`}
+    >
       <Container className={styles.headerContainer}>
         <Navbar.Brand>
-            <Link to="main  ">Logo</Link>
+          <Link to="main">Logo</Link>
         </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="main" className={styles.navLink}>Главная</Link>
-            <Link to="about" className={styles.navLink}>О нас</Link>
-            <Link to="entry" className={styles.navLink}>Запись</Link>
-            <Link to="contacts" className={styles.navLink}>Контакты</Link>
+            <Link to="main" className={styles.navLink}>
+              Главная
+            </Link>
+            <Link to="about" className={styles.navLink}>
+              О нас
+            </Link>
+            <Link to="entry" className={styles.navLink}>
+              Запись
+            </Link>
+            <Link to="contacts" className={styles.navLink}>
+              Контакты
+            </Link>
           </Nav>
         </Navbar.Collapse>
-        <Button className={styles.signinBtn} variant="success">Войти</Button>
-
+        <Button className={styles.signinBtn} variant="success">
+          Войти
+        </Button>
       </Container>
     </Navbar>
   );
