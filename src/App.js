4156c605db05c9e69bc5bb67be378cce6dbfe72f
TimurKhAky <timur.khaky@list.ru>
2022-11-17 09:57:43 +0300

    верстка страницы регистрации

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import "./App.scss";
 import MainPage from "./pages/MainPage/MainPage";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
+import SignUp from "./components/Authorization/SignUp/SignUp";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Route>
+    </Routes>
   );
 }
 
