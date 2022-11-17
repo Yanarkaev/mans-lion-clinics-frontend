57d3463552bd525e94cd481f714069b03273cd08
TimurKhAky <timur.khaky@list.ru>
2022-11-17 09:44:20 +0300

    верстка страницы регистрации

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import  './App.scss'
-import Header from './components/Header/Header';
+import "./App.scss";
+import SignUp from "./components/Authorization/SignUp/SignUp";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div>
-    <Header />
-  </div>;
+  return (
+    <div>
+      <Header />
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
