b15642e062a34ce8246c61b0e084423a6cb309b0
Laziyni <soasue@mail.ru>
2022-11-18 10:16:24 +0300

    Завершена верстка страницы отделений

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout/Layout";
 import SignUp from "./components/Authorization/SignUp/SignUp";
 import AboutPage from "./pages/About/AboutPage";
+import Departments from "./pages/Departments/Departments";
+// import SignIn from "./components/Authorization/SignIn/SignIn";
 
 function App() {
   return (
@@ -11,10 +13,11 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<MainPage />} />
         <Route path="/signup" element={<SignUp />} />
+        {/* <Route path="/signin" element={<SignIn />} /> */}
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/entry" element={<Departments />} />
       </Route>
     </Routes>
   );
 }
-
 export default App;
