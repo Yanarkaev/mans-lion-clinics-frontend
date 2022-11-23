f45139c5b86926321260321f5d934ce032852a2e
Yanarkaev <hasssanbek@gmail.com>
2022-11-23 22:44:08 +0300

    сверстал (е) чат для админа и генератор кода

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Sad from "./components/Authorization/Sad/Sad";
 import SignUpWorker from "./components/Authorization/SignUpWorker/SignUpWorker";
 import SingleDoctor from "./pages/SingleDoctor/SingleDoctor";
 import ContactsPage from "./pages/Contacts/ContactsPage";
+import AdminPage from './pages/AdminPage/AdminPage';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         <Route path="/entry" element={<Departments />} />
         <Route path="/entry/doctor/:id" element={<SingleDoctor />} />
         <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/admin" element={<AdminPage />} />
       </Route>
     </Routes>
   );
--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./adminPage.module.scss";
+import AdminChat from './../../components/AdminChat/AdminChat';
 
 const AdminPage = () => {
+  const [job, setJob] = useState(false);
+  const [invCode, setInvCode] = useState("");
+
+  const generCode = () => {
+    const symbols =
+      "123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    let password = "";
+    for (var i = 0; i <= 10; i++) {
+      const randomNumber = Math.floor(Math.random() * symbols.length);
+      password += symbols.substring(randomNumber, randomNumber + 1);
+    }
+    setInvCode(password);
+  };
+
   return (
     <div className={s.wrapper}>
       <div className={s.adminCard}>
@@ -13,6 +28,33 @@ const AdminPage = () => {
           <div>ты админ, бро</div>
         </div>
       </div>
+      <div className={s.invationWrapper}>
+        <div className={s.invationInner}>
+          <h5>Пригласительный код</h5>
+          <input
+            type="text"
+            value={invCode}
+            onChange={(e) => setInvCode(e.target.value)}
+            placeholder="Введите код"
+          />
+
+          <div className={s.sendWrapper}>
+            <button className={s.send}>Отправить</button>
+            <button className={s.gener} onClick={generCode}>
+              Сгенерировать
+            </button>
+          </div>
+        </div>
+
+        <div className={s.choiceWrapper}>
+          <h5>Должность:</h5>
+          <button className={s.toggleJob} onClick={() => setJob(!job)}>
+            {job ? "Врач" : "Админ"}
+          </button>
+        </div>
+      </div>
+
+     <AdminChat />
     </div>
   );
 };
